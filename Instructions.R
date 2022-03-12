a067284aedeee71796174d8261e7571b40977fc4
kbardwel <98776417+kbardwel@users.noreply.github.com>
2022-03-11 21:45:20 -0500

    Added Instructions

---
+++ Instructions.R
@@ -0,0 +1,57 @@
+
+
+#INSTRUCTIONS
+
+
+#STOCKS!
+  
+#Welcome to our shiny app all about visualizing and comparing stocks to 
+#help the user make informed decisions about their investments, to gain knowledge 
+#about stocks, or to explore their curiosity about stocks.
+
+#Visualizing a Stock: To use this feature the user must first click the arrow 
+#under the widget labeled "Select Stock". Next, the user may scroll through the 
+#alphabetical list of stock ticker symbols and select their desired stock. This 
+#will allow the user to begin to narrow down what information they would like to 
+#view. Next, under the “Select Metric” widget, the user can scroll through the 
+#options and select a metric to visualize within their selected stock, thus 
+#narrowing down their selection further. Next, under the “Select Date Range” 
+#widget, the user can choose the date range they would like to view by selecting 
+#the left date as the beginning of their range, and the right date as the ending 
+#of their range. This allows the user to narrow down their data to the timeframe 
+#they would like to view. After selecting all of these, a plot will appear for 
+#the user to visualize their selected stock and metric during the selected date range.
+
+
+#Compare Stocks: To use this feature the user must first click the arrow under the 
+#widget labeled "Select Stock". Next, the user may scroll through the alphabetical 
+#list of stock ticker symbols and select their desired stock. The user will repeat 
+#this step under the second “Select Stock” widget with the stock they would like to 
+#compare their first selected stock with. Next, under the “Select Metric” widget, the 
+#user can scroll through the options and select a metric to visualize within their 
+#selected stocks, thus narrowing down their selection further. Next, under the 
+#“Select Date Range” widget, the user can choose the date range they would like to view 
+#by selecting the left date as the beginning of their range, and the right date as the 
+#ending of their range. This allows the user to narrow down their data to the timeframe 
+#they would like to view. After selecting all of these, a plot will appear for the user 
+#to visualize their selected stocks and metric during the selected date range for a 
+#visual comparison between these stocks.
+
+#Purchase a Stock: To use this feature the user must first click the arrow under the 
+#widget labeled "Select Stock". Next, the user may scroll through the alphabetical list 
+#of stock ticker symbols and select their desired stock. This will allow the user to 
+#begin to narrow down what information they would like to view. Next, under the 
+#“How Many Shares?” widget, the user can slide the slider to select how many stocks 
+#they would hypothetically like to purchase. Then, under the “Select Buy Date” widget, 
+#the user can select the date they would hypothetically buy the stock. Under the 
+#“Select New Date” widget, the user can select a date after the buy date where they 
+#would sell the stock. This will then output the growth or decline of the stock to see 
+#if the stock is worth purchasing.
+
+#View Open and Closed Differences: To use this feature the user must first click the arrow 
+#under the widget labeled "Select a Symbol". Next, the user may scroll through the 
+#alphabetical list of stock ticker symbols and select their desired stock. Next, under 
+#the “Input a Date” widget, the user can select the date that they would like to view as 
+#their start in the open and closed difference plots. After these have been selected, the 
+#user can press the “Go!” button that will output a plot of the percent change in the open 
+#and closed differences of their selected stock.
